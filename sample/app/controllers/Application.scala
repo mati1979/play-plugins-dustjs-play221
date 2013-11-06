5d141816269107e54b6c1afbae5d206161217e91
Mateusz Szczap <mszczap@team.mobile.de>
2013-11-06 10:03:39 +0100

    added more intelligent implementation of serializing json data
    Change-Id: Iead258a0c77e63af44f5af9d1728fe257c62b0d1

--- sample/app/controllers/Application.scala
+++ sample/app/controllers/Application.scala
@@ -2,21 +2,20 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object Application extends Controller {
+
+  case class Inbox(val name: String, count: Int)
+  implicit val inboxFormat = Json.format[Inbox]
 
   def index = Action {
     Ok(views.html.index())
   }
 
   def data = Action {
-    import play.api.libs.json._
-    Ok(
-      Json.obj(
-        "name" -> "Json",
-        "count" -> 1
-      )
-    )
+    Ok(Json.toJson(Inbox("Json", 12)))
   }
 
 }
